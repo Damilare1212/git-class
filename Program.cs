1cfb9eb3b0f80cd7cbf5f11ae0c693574e4c8219
yusuf masroor ahmad <DamilareLamina@gmail.com>
2021-11-15 11:59:53 +0100

    crate a new projet

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gitClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("masroor ahmad");
+        }
+    }
+}
